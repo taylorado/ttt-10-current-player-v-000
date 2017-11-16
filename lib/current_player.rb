8e17ba05d8dc8efbb5ea4cc38998337ea98bb9a5
Taylor Donlan <taylordonlan@gmail.com>
2017-11-16 01:52:21 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,11 +2,11 @@
 
 def turn_count(board)
   return board.count("X") + board.count("O")
-
+#aleternatively, use each iterator and increase count by one when encountering "X" or "O"
 end
 
 
-def current_player
+def current_player(board)
   if turn_count(board).even?
     current_player = "X"
   else
